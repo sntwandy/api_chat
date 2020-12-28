415db0aed8cd0bba1e03918f3eefc6f784cdf348
sntwandy <sntwandy@gmail.com>
2020-12-28 16:46:20 -0400

    using router to handle get, post etc

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
+'use strict';
+
 const express = require('express');
+const router = express.Router();
 
 const app = express();
 // eslint-disable-next-line no-undef
 const port = process.env.port || 3000;
 
-app.use('/', (request, response) => {
-  response.send('Hello! :)');
+app.use(router);
+
+router.get('/message', (req, res) => {
+  res.send('Messages lists');
 });
+
+router.post('/message', (req, res) => {
+  res.send('Message added!');
+});
+
+// app.use('/', (request, response) => {
+//   response.send('Hello! :)');
+// });
+
+
 
 app.listen(port, () => console.log(`The App is listening on: http://localhost:${port}/`));
