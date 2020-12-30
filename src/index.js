d79ba4eb193af9ce11f510ce32eb398f8011a4c7
sntwandy <sntwandy@gmail.com>
2020-12-30 01:39:13 -0400

    handling parameters with body and query

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
 
 const app = express();
 // eslint-disable-next-line no-undef
 const port = process.env.port || 3000;
 
+app.use(bodyParser.json());
 app.use(router);
 
 router.get('/message', (req, res) => {
-  res.send('Messages lists');
+  console.log(req.body);
+  console.log(req.query);
+  res.send(`Messages ${req.body.name} lists`);
 });
 
 router.post('/message', (req, res) => {
