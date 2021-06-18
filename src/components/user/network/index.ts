342a0f439f78892424de140fc288ff9ea11d2256
Wandy Santana <sntwandy@gmail.com>
2021-06-18 05:18:49 -0400

    Fix

--- src/components/user/network/index.ts
+++ src/components/user/network/index.ts
@@ -1,13 +1,13 @@
 /**
  *
  */
-import { Request, Response ,Router } from 'express';
+import { Request, Response, Router } from 'express';
 import controller from '../controller';
 import response from '../../../network/response';
 
 const router = Router();
 
-// Add a user
+// add a user
 router.post('/', async (req: Request, res: Response): Promise<void> => {
   const user = req.body;
   user.registerDate = new Date();
@@ -19,7 +19,7 @@ router.post('/', async (req: Request, res: Response): Promise<void> => {
   }
 });
 
-// Get all uses
+// get all uses
 router.get('/', async (req: Request, res: Response) => {
   try {
     const users = await controller.getAllUsers();
