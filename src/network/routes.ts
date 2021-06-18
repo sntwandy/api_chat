07f9b803be8457a5fc0db68a3737871398725e6c
Wandy Santana <sntwandy@gmail.com>
2021-06-18 05:18:06 -0400

    Update routes with api word

--- src/network/routes.ts
+++ src/network/routes.ts
@@ -3,11 +3,10 @@ import message from '../components/message/network';
 import user from '../components/user/network';
 import chat from '../components/chat/network';
 
-
 const routes = (server: Application) => {
-  server.use('/message', message);
-  server.use('/user', user);
-  server.use('/chat', chat);
+  server.use('/api/message', message);
+  server.use('/api/user', user);
+  server.use('/api/chat', chat);
 };
 
 export default routes;
