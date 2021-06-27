10416757a6a7265c4b4a6c2513908971c570f0a4
sntwandy <sntwandy@gmail.com>
2021-06-27 09:22:44 -0400

    update routes

--- src/network/routes.ts
+++ src/network/routes.ts
@@ -5,9 +5,9 @@ import chat from '../components/chat/network';
 
 
 const routes = (server: Application) => {
-  server.use('/message', message);
-  server.use('/user', user);
-  server.use('/chat', chat);
+  server.use('/api//message', message);
+  server.use('/api//user', user);
+  server.use('/api//chat', chat);
 };
 
 export default routes;
